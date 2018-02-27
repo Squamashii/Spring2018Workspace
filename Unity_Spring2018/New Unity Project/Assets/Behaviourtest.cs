8551c6f2c840cb354af8b4041f274a0dd1dab9bf
Unknown <spencercbarber@gmail.com>
2018-02-27 13:39:36 -0700

    Setting Up Unity for the semester

---
+++ Unity_Spring2018/New Unity Project/Assets/Behaviourtest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RoboRyanTron.Unite2017.Events;
+
+public class Behaviourtest : StateMachineBehaviour {
+
+public GameEvent Event;
+
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		Event.Raise();
+	}
+
+
+
+
+}

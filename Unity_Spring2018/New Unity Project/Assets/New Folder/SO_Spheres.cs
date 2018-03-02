d493e53bcd26c8ff21125feb098bfd74c8e3a7a5
Unknown <spencercbarber@gmail.com>
2018-03-02 14:57:20 -0700

    Goblin State Machine

---
+++ Unity_Spring2018/New Unity Project/Assets/New Folder/SO_Spheres.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Sphere", menuName = "Scriptable Ball")]
+
+public class SO_Spheres : ScriptableObject {
+
+	public new string name;
+	public Color color;
+	public Transform scale;
+
+
+
+}

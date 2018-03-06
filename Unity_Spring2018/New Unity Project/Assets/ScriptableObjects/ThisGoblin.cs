729622685890a70a908522a069c3e3d3d247ca0a
Unknown <spencercbarber@gmail.com>
2018-03-06 12:19:49 -0700

    RE-did the scriptable object stuff

---
+++ Unity_Spring2018/New Unity Project/Assets/ScriptableObjects/ThisGoblin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RoboRyanTron.Unite2017.Events;
+
+public class ThisGoblin : MonoBehaviour {
+
+	public Renderer rend;
+	public GoblinDeets GoblinSO;
+	public float speed;
+
+	void Start(){
+		rend = GetComponent<Renderer>();
+	}
+
+	void Update(){
+		rend.material.color = GoblinSO.skinColor;
+		speed = GoblinSO.speed;
+	}
+}

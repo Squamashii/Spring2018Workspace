df341981f6287c36998bcb9b863a76ab77b9a851
Unknown <spencercbarber@gmail.com>
2018-03-08 16:38:54 -0700

    Jim Head Modeling, and Hazard scripting

--- Unity_Spring2018/TechTeamPlayground/Assets/TimedHazard.cs
+++ Unity_Spring2018/TechTeamPlayground/Assets/TimedHazard.cs
@@ -11,27 +11,26 @@ public class TimedHazard : StateMachineBehaviour {
 	public int minRange;
 	public int maxRange;
 	float timer = 0;
+	public string triggerParameter;
 	
-	public GameEvent triggerEvent; //An event that will get triggered onExit
-	
-	public void RandGenerator()
+	public void RandGenerator() //This method generates a random number within the set range
 	{
 		randNum = Random.Range (minRange, maxRange);
 	}
 	
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
 	{
-		startTimer = Time.time;
+		startTimer = Time.time; //Starts counting seconds.  
 		RandGenerator();
 	}
 
 	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
 	{
-		timer = Time.time - startTimer;
+		timer = Time.time - startTimer; //
 
-		if(timer > randNum)
+		if(timer > randNum) 
 		{
-			triggerEvent.Raise();
+			animator.SetTrigger(triggerParameter);
 		}
 
 		timer = 0;
